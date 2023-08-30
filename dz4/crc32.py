e3f76f7509fc7d924cb4809b8fe81a99ca84fb06
Anna.K <kkk.aanya1996@mail.ru>
2023-08-30 16:40:07 +0300

    задание 3 и 4

--- dz4/crc32.py
+++ dz4/crc32.py
@@ -1,11 +1,14 @@
 import zlib
 
 def crc32(fileName):
-    # Контрольная сумма файла CRC-32 // :param fileName: Путь к файлу
+    """
+    Контрольная сумма файла CRC-32
+    :param fileName: Путь к файлу
+    """
     with open(fileName, 'rb') as fh:
         hash = 0
         while True:
-            s = fh.read(65536)
+            s = fh.read(77852)
             if not s:
                 break
             hash = zlib.crc32(s, hash)
